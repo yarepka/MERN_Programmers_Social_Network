17582300fc3090d4a1c7fe7ba9aef0bc09102fea
Роман Бубнов <yarepka01@gmail.com>
2020-07-30 10:04:57 +0300

    Registration & Login & Logout done

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -9,25 +9,33 @@ import './App.css';
 // Redux
 import { Provider } from 'react-redux'
 import store from './store';
+import { loadUser } from './actions/auth';
 
 // Fragment is ghost component, will not be show in the DOM
-const App = () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <section className="container">
-          <Alert />
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </BrowserRouter>
-  </Provider>
-);
+const App = () => {
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,11 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -24,8 +25,14 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert('Passwords do not match', 'danger');
     } else {
-      console.log('SUCCESS');
+      register({
+        name, email, password
+      });
     }
+  }
+
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />
   }
 
   return (
@@ -43,7 +50,6 @@ const Register = ({ setAlert }) => {
             name="name"
             value={name}
             onChange={e => onChangeHandler(e)}
-            required
           />
         </div>
 
@@ -54,7 +60,6 @@ const Register = ({ setAlert }) => {
             name="email"
             value={email}
             onChange={e => onChangeHandler(e)}
-            required
           />
 
           <small className="form-text">
@@ -66,7 +71,6 @@ const Register = ({ setAlert }) => {
           <input
             type="password"
             placeholder="Password"
-            minLength="6"
             name="password"
             value={password}
             onChange={e => onChangeHandler(e)}
@@ -77,7 +81,7 @@ const Register = ({ setAlert }) => {
           <input
             type="password"
             placeholder="Confirm Password"
-            minLength="6" name="password2"
+            name="password2"
             value={password2}
             onChange={e => onChangeHandler(e)}
           />
@@ -97,6 +101,12 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
 };
 
-export default connect(null, { setAlert })(Register);
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
