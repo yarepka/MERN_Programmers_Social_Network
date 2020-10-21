1166b05dbad2741238c3b0ddafce1d42e9d1e9aa
Roman Bubnov <yarepka01@gmail.com>
2020-10-22 01:59:11 +0300

    Get user repos route updated

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -381,13 +381,14 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 router.get('/github/:username', (req, res) => {
   try {
     const options = {
-      uri: encodeURI(
-        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
-      ),
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
       method: 'GET',
       headers: {
         'user-agent': 'node.js',
-        Authorization: `token ${config.get('githubToken')}`,
       },
     };
 
